88813a16fd1ff5562c2a7eeb3dc96eebd5fd45e0
Ramiro Garcia <ramirogarciaweb@gmail.com>
2021-04-21 02:56:32 -0300

    [Add] changes

--- main.js
+++ main.js
@@ -5,5 +5,13 @@ button.addEventListener("click", () => {
   // Creamos un h1 para despues saludar
   const name = document.createElement("h1");
   name.innerHTML = `Bienvenido a Cypress, ${input.value}`;
+  name.classList.add("box");
+  name.id = "greeting";
   document.body.appendChild(name);
+
+  setTimeout(() => {
+    input.value = "";
+    const padre = name.parentNode;
+    padre.removeChild(name);
+  }, 5000);
 });
